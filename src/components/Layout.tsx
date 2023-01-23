6b85a8286738074dba399e56379b92571f9e2019
Gustavo Peña <gustavo.pema@gmail.com>
2023-01-23 16:53:38 -0600

    fix: change navbar favorites label

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ function Layout({ children }: any) {
     <div className='bg-gray-100 min-h-screen'>
       <div className='sticky top-0 max-w-6xl mx-auto bg-white rounded-b-lg p-4 text-slate-800 font-bold uppercase z-10 mb-4 flex space-x-4'>
         <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites - { Object.keys(favorites).length }</Link>
+        <Link to="/favorites">Favorites({ Object.keys(favorites).length })</Link>
       </div>
       <div className='relative'>
         <img className='fixed top-[-200px] left-[-200px] w-[540px] h-[540px] -rotate-12 text-red-600' src={PokeballImg} alt="pokeball" />
