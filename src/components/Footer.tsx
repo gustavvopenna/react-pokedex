f4f6ad966b06e8d4daeb21ad60674330a7b645d1
Gustavo Peña <gustavo.pema@gmail.com>
2023-01-27 16:26:00 -0600

    fix: footer copy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import PokeballImg from '../assets/images/pokeball.svg'
 export default function Footer() {
   return (
     <footer className="relative text-sm text-slate-700 font-semibold py-2 px-4 bg-white">
-      Made with by <a href="https://github.com/gustavvopenna">Gus</a> with ❤️ and ⚛
+      Made with ❤️ and ⚛ by <a href="https://github.com/gustavvopenna">Gus</a>.
       <img src={PokeballImg} alt="pokeball" className='absolute top-[-24px] right-4 w-12 h-12' />
     </footer>
   )
