a96124396b9248907d5cbaf890c60ff07b8fccb1
Gustavo Peña <gustavo.pema@gmail.com>
2023-01-30 12:26:24 -0600

    fix: Simplify store types

--- src/store/favoritesStore.tsx
+++ src/store/favoritesStore.tsx
@@ -3,36 +3,31 @@ import { persist } from 'zustand/middleware'
 
 const DEFAULT_SELECTED = { url: "https://pokeapi.co/api/v2/pokemon/1/", name: "bulbasaur" }
 
-interface Iselected {
-  url: string,
-  name: string
-}
-
-interface IfavoriteItem {
-  id: string,
+interface FavoriteItem {
+  id?: string,
   name: string,
   url: string,
 }
 
-interface IfavoritesState {
+interface FavoritesState {
   favorites: {},
-  selected: Iselected,
-  setSelected: (item: Iselected) => void,
-  addFavorite: (item: IfavoriteItem) => void,
-  removeFavorite: (item) => void,
+  selected: FavoriteItem,
+  setSelected: (item: FavoriteItem) => void,
+  addFavorite: (item: FavoriteItem) => void,
+  removeFavorite: (item: { id: string }) => void,
 }
 
 export const useFavoritesStore = create<
-  IfavoritesState,
+  FavoritesState,
   [
-    ['zustand/persist', IfavoritesState]
+    ['zustand/persist', FavoritesState]
   ]
 >(
   persist(
     (set) => ({
       favorites: {},
       selected: DEFAULT_SELECTED,
-      setSelected: (item) => set(() => ({ selected: item })),
+      setSelected: item => set(() => ({ selected: item })),
       addFavorite: item => set(state => ({ favorites: { ...state.favorites, [item.id]: { name: item.name, url: item.url } } })),
       removeFavorite: item => set(state => {
         const { [item.id]: itemToRemove, ...rest } = state.favorites
